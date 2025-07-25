7fc9285e2725c25697751a86dd5aa482cb0bccd5
Rob Glen <rob.glen@hotmail.co.uk>
2025-07-25 13:11:47 +0100

    Adding headers for days and the 2 parts.

--- Source/Day.cpp
+++ Source/Day.cpp
@@ -4,8 +4,15 @@
 
 void Day::RunDay()
 {
+	const std::string dayId = GetDayId();
+
+	std::cout << dayId << " Part 1" << std::endl;
 	RunDayPart1();
+
+	std::cout << std::endl << dayId << " Part 2" << std::endl;
 	RunDayPart2();
+
+	std::cout << std::endl;
 }
 
 void Day::DebugReport(const std::string& inString) const
